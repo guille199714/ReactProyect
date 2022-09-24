06819a48cd7df28f969d5eceb2abe49c7f81a06d
Guillermo Ithurbide <guille199714@gmail.com>
2022-09-23 22:43:05 -0300

    novos assign activities

--- src/components/ActivitiesList.tsx
+++ src/components/ActivitiesList.tsx
@@ -2,21 +2,21 @@ import React from 'react'
 
 import { CloseOutlined } from '@ant-design/icons'
 import { List, Button } from 'antd'
-import VirtualList from 'rc-virtual-list';
-
+import VirtualList from 'rc-virtual-list'
 
 type ActivitiesListProps = {
-  data: RecordType[],
-  isSelectedActivity?: boolean,
-  setRecord: (value: RecordType) => void,
-  removeRecord: (value: RecordType) => void,
+  data: RecordType[]
+  isSelectedActivity?: boolean
+  setRecord: (value: RecordType) => void
+  removeRecord: (value: RecordType) => void
 }
 
 type RecordType = {
-  title: string;
-  materia: String;
-  secuencia: String;
-  unidad: String;
+  title: string
+  area: string
+  materia: string
+  secuencia: string
+  unidad: string
 }
 
 const ActivitiesList: React.FC<ActivitiesListProps> = (props) => {
@@ -30,7 +30,7 @@ const ActivitiesList: React.FC<ActivitiesListProps> = (props) => {
         data={props.data}
         height={400}
         itemHeight={50}
-        itemKey="title"
+        itemKey='title'
         style={{ padding: '1vh 0.8vw' }}
       >
         {(item: RecordType) => (
@@ -39,17 +39,24 @@ const ActivitiesList: React.FC<ActivitiesListProps> = (props) => {
             key={item.title}
             style={{ textAlign: 'center', cursor: 'pointer', border: '1px solid #f0f0f0' }}
           >
-            <List.Item.Meta
-              title={item.title}
-            />
-            { props.isSelectedActivity &&
-              (<Button type="primary" style={{position: 'absolute', marginTop: '-3.4vh', marginLeft: '12vw' }} onClick={() => props.removeRecord(item)} key={item.title} danger shape="circle" size={"small"} icon={<CloseOutlined />} />)
-            }
+            <List.Item.Meta title={item.title} />
+            {props.isSelectedActivity && (
+              <Button
+                type='primary'
+                style={{ position: 'absolute', marginTop: '-3.4vh', marginLeft: '12vw' }}
+                onClick={() => props.removeRecord(item)}
+                key={item.title}
+                danger
+                shape='circle'
+                size={'small'}
+                icon={<CloseOutlined />}
+              />
+            )}
           </List.Item>
         )}
       </VirtualList>
     </List>
-  );
+  )
 }
 
 export default ActivitiesList
